7457a81b18bb5a55dd9d5db9ce26d0a0384de579
Erik Fleckstein <erik@tinkerforge.com>
2020-08-27 15:30:04 +0200

    uc: Fix warnings.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -5,7 +5,11 @@
 
 void check(int rc, const char* msg);
 
-TF_DistanceIRV2 dir;
+void example_setup(TF_HalContext *hal);
+void example_loop(TF_HalContext *hal);
+
+
+static TF_DistanceIRV2 dir;
 
 void example_setup(TF_HalContext *hal) {
 	// Create device object
